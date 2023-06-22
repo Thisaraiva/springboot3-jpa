b8a536dcaf86618b9f4b0e7eac0929fd559ce23e
Thiago Saraiva <thiagofreitassaraiva@yahoo.com.br>
2023-06-21 23:33:07 -0300

    Service layer, component registration

--- src/main/java/com/thiagojavacourse/course/resources/UserResource.java
+++ src/main/java/com/thiagojavacourse/course/resources/UserResource.java
@@ -1,22 +1,36 @@
 package com.thiagojavacourse.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.thiagojavacourse.course.entities.User;
+import com.thiagojavacourse.course.services.UserService;
 
 @RestController//anotation 
 @RequestMapping(value = "/users") // nome dado ao recurso /users
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	//endpoint criado para acessar os recursos de users
 	@GetMapping
-	public ResponseEntity<User> findAll(){// retorna respostas de requisições web
-		User u = new User(1L, "Thiago", "thiago@gmail.com", "99999999", "12345");
-		return ResponseEntity.ok().body(u);
-				
+	public ResponseEntity<List<User>> findAll(){// retorna respostas de requisições web
+		List<User> list = service.findAll();		
+		return ResponseEntity.ok().body(list);				
 	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
 
 }
---
+++ src/main/java/com/thiagojavacourse/course/services/UserService.java
@@ -0,0 +1,27 @@
+package com.thiagojavacourse.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.thiagojavacourse.course.entities.User;
+import com.thiagojavacourse.course.repositories.UserRepository;
+
+@Service //anotation "component" que já registra sua classe como um componente do spring e ela já poderá ser injetada com o @Autowired, mas como é uma classe de serviço usaremos o anotation @Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
