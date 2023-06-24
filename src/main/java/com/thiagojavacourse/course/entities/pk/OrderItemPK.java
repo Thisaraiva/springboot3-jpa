727e9ea1ac7d3527be8f762757869653dd4b8679
Thiago Saraiva <thiagofreitassaraiva@yahoo.com.br>
2023-06-23 23:03:28 -0300

    OrderItem, many-to-many association with extra attributes PART 1

---
+++ src/main/java/com/thiagojavacourse/course/entities/pk/OrderItemPK.java
@@ -0,0 +1,55 @@
+package com.thiagojavacourse.course.entities.pk;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import com.thiagojavacourse.course.entities.Order;
+import com.thiagojavacourse.course.entities.Product;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+
+//Classe criada para integrar pedidos e produtos que são os itens de pedido, como ela não tem uma chave primaria para identifica-la orderItem então criamos essa classe
+@Embeddable
+public class OrderItemPK implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@ManyToOne
+	@JoinColumn(name = "order_id")
+	private Order order;
+	@ManyToOne
+	@JoinColumn(name = "product_id")
+	private Product product;
+	
+	//essa classe em específico não terá construtores
+	
+	public Order getOrder() {
+		return order;
+	}
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+	public Product getProduct() {
+		return product;
+	}
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(order, product);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderItemPK other = (OrderItemPK) obj;
+		return Objects.equals(order, other.order) && Objects.equals(product, other.product);
+	}
+}
