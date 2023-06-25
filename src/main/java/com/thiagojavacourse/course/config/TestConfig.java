350a59dadacfe868fab765caf97f25a79b44746e
Thiago Saraiva <thiagofreitassaraiva@yahoo.com.br>
2023-06-25 19:15:40 -0300

    Payment, one-to-one association

--- src/main/java/com/thiagojavacourse/course/config/TestConfig.java
+++ src/main/java/com/thiagojavacourse/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.thiagojavacourse.course.entities.Category;
 import com.thiagojavacourse.course.entities.Order;
 import com.thiagojavacourse.course.entities.OrderItem;
+import com.thiagojavacourse.course.entities.Payment;
 import com.thiagojavacourse.course.entities.Product;
 import com.thiagojavacourse.course.entities.User;
 import com.thiagojavacourse.course.entities.enums.OrderStatus;
@@ -80,5 +81,10 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
