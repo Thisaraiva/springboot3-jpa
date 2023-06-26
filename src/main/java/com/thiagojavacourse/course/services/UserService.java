5b048963ee92e84ceafb094b9c31c047d1e6a95f
Thiago Saraiva <thiagofreitassaraiva@yahoo.com.br>
2023-06-26 18:16:00 -0300

    Exception handling - delete

--- src/main/java/com/thiagojavacourse/course/services/UserService.java
+++ src/main/java/com/thiagojavacourse/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.thiagojavacourse.course.entities.User;
 import com.thiagojavacourse.course.repositories.UserRepository;
+import com.thiagojavacourse.course.services.exceptions.DatabaseException;
 import com.thiagojavacourse.course.services.exceptions.ResourceNotFoundException;
 
 @Service //anotation "component" que já registra sua classe como um componente do spring e ela já poderá ser injetada com o @Autowired, mas como é uma classe de serviço usaremos o anotation @Service
@@ -30,7 +33,13 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
